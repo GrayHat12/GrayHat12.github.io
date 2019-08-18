ae0584442c3ad0ad89860d6c086f00a3a38c2d14
GrayHat12 <grayhathacks10@gmail.com>
2019-08-18 22:06:07 +0530

    Update sendEmail.php

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -3,13 +3,13 @@ $str = $_POST;
 $params = array();
 parse_str($str,$params)
 $to = 'grayhathacks10@gmail.com'
-$name = $params['contactName']
-$email = 'From : '+$params['contactEmail']
-$subject = $params['contactSubject']
-$message = $params['contactMessage']
+$name = $params['form-field.contactName']
+$email = 'From : '+$params['form-field.contactEmail']
+$subject = $params['form-field.contactSubject']
+$message = $params['form-field.contactMessage']
 if(mail($to,$subject,$message,$email))
 {
-    echo("OK");
+    echo('OK');
 }
 else
 {
